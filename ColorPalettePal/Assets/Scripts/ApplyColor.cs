839375ed42b2a1d96094731a4efd5aaac74c9e47
Kewejuankenobie <97452684+Kewejuankenobie@users.noreply.github.com>
2024-10-22 16:05:57 -0400

    Can change color 1 with hex
    Add hex code to change color 1, color 1 does not change hex automatically yet

--- ColorPalettePal/Assets/Scripts/ApplyColor.cs
+++ ColorPalettePal/Assets/Scripts/ApplyColor.cs
@@ -1,43 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
+
 
 public class ApplyColor : MonoBehaviour
 {
     //Public Variables
-    public Material color1Material;
-    public Material color2Material;
-    public Material color3Material;
-    public Material color4Material;
-    public Material color5Material;
-    public Material color6Material;
-    public FlexibleColorPicker colorPicker;
+    public Material color1_material;
+    public Material color2_material;
+    public Material color3_material;
+    public Material color4_material;
+    public Material color5_material;
+    public Material color6_material;
+    public FlexibleColorPicker color_picker;
     
     //Private Variables
-    private bool canChangeCol1;
-    private bool canChangeCol2;
-    private bool canChangeCol3;
-    private bool canChangeCol4;
-    private bool canChangeCol5;
-    private bool canChangeCol6;
+    private bool can_change_col1;
+    private bool can_change_col2;
+    private bool can_change_col3;
+    private bool can_change_col4;
+    private bool can_change_col5;
+    private bool can_change_col6;
     private Color previousColor;
     
     // Start is called before the first frame update
     void Start()
     {
-        canChangeCol1 = true;
-        canChangeCol2 = true;
-        canChangeCol3 = true;
-        canChangeCol4 = true;
-        canChangeCol5 = true;
-        canChangeCol6 = true;
-        color1Material.color = colorPicker.color;
-        color2Material.color = colorPicker.color;
-        color3Material.color = colorPicker.color;
-        color4Material.color = colorPicker.color;
-        color5Material.color = colorPicker.color;
-        color6Material.color = colorPicker.color;
-        previousColor = colorPicker.color;
+        can_change_col1 = true;
+        can_change_col2 = true;
+        can_change_col3 = true;
+        can_change_col4 = true;
+        can_change_col5 = true;
+        can_change_col6 = true;
+        color1_material.color = color_picker.color;
+        color2_material.color = color_picker.color;
+        color3_material.color = color_picker.color;
+        color4_material.color = color_picker.color;
+        color5_material.color = color_picker.color;
+        color6_material.color = color_picker.color;
+        previousColor = color_picker.color;
     }
 
     // Update is called once per frame
@@ -45,67 +47,85 @@ public class ApplyColor : MonoBehaviour
     {
         //Checks if each of the colors can be changed, and the current color is not the previous color
         //If this holds, change the color
-        if (canChangeCol1 && !previousColor.Equals(colorPicker.color))
+        if (can_change_col1 && !previousColor.Equals(color_picker.color))
         {
-            color1Material.color = colorPicker.color;
+            color1_material.color = color_picker.color;
         }
 
-        if (canChangeCol2 && !previousColor.Equals(colorPicker.color))
+        if (can_change_col2 && !previousColor.Equals(color_picker.color))
         {
-            color2Material.color = colorPicker.color;
+            color2_material.color = color_picker.color;
         }
 
-        if (canChangeCol3 && !previousColor.Equals(colorPicker.color))
+        if (can_change_col3 && !previousColor.Equals(color_picker.color))
         {
-            color3Material.color = colorPicker.color;
+            color3_material.color = color_picker.color;
         }
 
-        if (canChangeCol4 && !previousColor.Equals(colorPicker.color))
+        if (can_change_col4 && !previousColor.Equals(color_picker.color))
         {
-            color4Material.color = colorPicker.color;
+            color4_material.color = color_picker.color;
         }
 
-        if (canChangeCol5 && !previousColor.Equals(colorPicker.color))
+        if (can_change_col5 && !previousColor.Equals(color_picker.color))
         {
-            color5Material.color = colorPicker.color;
+            color5_material.color = color_picker.color;
         }
 
-        if (canChangeCol6 && !previousColor.Equals(colorPicker.color))
+        if (can_change_col6 && !previousColor.Equals(color_picker.color))
         {
-            color6Material.color = colorPicker.color;
+            color6_material.color = color_picker.color;
         }
 
-    previousColor = colorPicker.color;
+    previousColor = color_picker.color;
     }
 
     //Functions to monitor whether a color is toggled or not
     public void toggleColor1(bool canChange1)
     {
-        canChangeCol1 = !canChangeCol1;
+        can_change_col1 = !can_change_col1;
     }
 
     public void toggleColor2(bool canChange2)
     {
-        canChangeCol2 = !canChangeCol2;
+        can_change_col2 = !can_change_col2;
     }
 
     public void toggleColor3(bool canChange3)
     {
-        canChangeCol3 = !canChangeCol3;
+        can_change_col3 = !can_change_col3;
     }
 
     public void toggleColor4(bool canChange4)
     {
-        canChangeCol4 = !canChangeCol4;
+        can_change_col4 = !can_change_col4;
     }
 
     public void toggleColor5(bool canChange5)
     {
-        canChangeCol5 = !canChangeCol5;
+        can_change_col5 = !can_change_col5;
     }
 
     public void toggleColor6(bool canChange6)
     {
-        canChangeCol6 = !canChangeCol6;
+        can_change_col6 = !can_change_col6;
+    }
+    
+    //Functions to change colors based on hex input
+    public void changeCol1Hex(string changed)
+    {
+        if (changed.Length == 7)
+        {
+            color1_material.color = createColor(changed);   
+        }
+    }
+    
+    //Private functions used by others
+    private Color createColor(string hex)
+    {
+        var red = int.Parse(hex.Substring(1, 2), NumberStyles.HexNumber) / 255f;
+        var green = int.Parse(hex.Substring(3, 2), NumberStyles.HexNumber) / 255f;
+        var blue = int.Parse(hex.Substring(5, 2), NumberStyles.HexNumber) / 255f;
+        return new Color(red, green, blue);
     }
 }
