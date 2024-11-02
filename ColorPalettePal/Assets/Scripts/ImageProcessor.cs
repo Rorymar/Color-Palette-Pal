caaedbff602eb14df2c687a19b4327a554950fb0
Rorymar <36866664+Rorymar@users.noreply.github.com>
2024-11-02 10:51:50 -0400

    Updated the Palette From Image Tool
    Updated the Palette from Image tool to be more strict about its contrast, and then also incorporated the new UI model.

--- ColorPalettePal/Assets/Scripts/ImageProcessor.cs
+++ ColorPalettePal/Assets/Scripts/ImageProcessor.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
+
 
 public class ImageProcessor : MonoBehaviour
 {
     public RawImage rawImage; // Assign in the Inspector
     private Texture previousTexture;
     public Material[] materials; // Array to hold your materials
+    public TMP_InputField[] inputFields;
     
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,14 @@ public class ImageProcessor : MonoBehaviour
         previousTexture = rawImage.texture;
 
         //DELETE WHEN CACHE IS SET UP(?)
+        for (int i = 0; i < materials.Length; i++){
+            materials[i].color = Color.white;
+            inputFields[i].SetTextWithoutNotify(createHexFromColor(materials[i].color));
+        }
+    }
+
+    void OnApplicationQuit()
+    {
         foreach (var material in materials)
         {
             material.color = Color.white;
@@ -55,7 +67,7 @@ public class ImageProcessor : MonoBehaviour
         AnalyzeColors(pixels);
     }
 
-    private void AnalyzeColors(Color[] pixels)
+    private void AnalyzeColors(Color[] pixels, int colorsToGrab = 6)
     {
         //Create a way of storing the color data
         Dictionary<Color, int> colorCounts = new Dictionary<Color, int>();
@@ -64,7 +76,6 @@ public class ImageProcessor : MonoBehaviour
         foreach (var pixel in pixels)
         {
             //Eliminate excessive similarity.
-            //WILL NEED TO STRENGTHEN OR EXPAND ON
             Color simplifiedPixel = new Color(
             Mathf.Round(pixel.r * 255) / 255,
             Mathf.Round(pixel.g * 255) / 255,
@@ -82,18 +93,69 @@ public class ImageProcessor : MonoBehaviour
             }
         }
 
-        //List the top five colors
-        var topColors = colorCounts.OrderByDescending(c => c.Value)
-                               .Take(6)
-                               .Select(c => c.Key)
-                               .ToList();
+        //Sort the colors by their frequency
+        var sortedColors = colorCounts.OrderByDescending(kvp => kvp.Value)
+                                          .Select(kvp => kvp.Key)
+                                          .ToList();
+        
+        //Starting threshold of color difference.
+        float threshold = 0.3f;
+
+        //The list that will hold the colors
+        List<Color> selectedColors = new List<Color>();
+
+        //Run through the colors, getting a list of six frequent colors that aren't overly similar
+        while(selectedColors.Count < colorsToGrab && threshold > 0){
+
+            //Clear the list
+            selectedColors.Clear();
+
+            //Loop through the colors till you have six or run out
+            foreach (var color in sortedColors){
+                if(selectedColors.Count < colorsToGrab)
+                {
+                    //Only add if sufficiently different from the previous color.
+                    if(selectedColors.All(c => ColorDifference(c,color) > threshold))
+                    {
+                        selectedColors.Add(color);
+                        materials[selectedColors.Count-1].color = color;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            //If failed to complete a palette, lower the threshold and try again.
+            threshold -= 0.05f;
+        }
+
+        //Assign the six colors
         for (int i = 0; i < materials.Length; i++)
         {
-            if (i < topColors.Count)
+            if (i < colorsToGrab)
             {
-                materials[i].color = topColors[i];
-                Debug.Log($"Assigned Color: {topColors[i]} to Material: {materials[i].name}");
+                materials[i].color = selectedColors[i];
+                inputFields[i].SetTextWithoutNotify(createHexFromColor(materials[i].color));
+                Debug.Log($"Assigned Color: {selectedColors[i]} to Material: {materials[i].name}");
             }
         }
     }
+
+    //Get hex code of a color
+    private string createHexFromColor(Color color)
+    {
+        int red = (int)(color.r * 255);
+        int green = (int)(color.g * 255);
+        int blue = (int)(color.b * 255);
+        string hex = "#" + red.ToString("X2") + green.ToString("X2")
+                     + blue.ToString("X2");
+        return hex.ToUpper();
+    }
+
+    //Calculate the degree of similarity between two colors.
+    private float ColorDifference(Color a, Color b){
+        return Mathf.Sqrt(Mathf.Pow(a.r - b.r, 2) + Mathf.Pow(a.g-b.g,2)+Mathf.Pow(a.b-b.b,2));
+    }
+
 }
