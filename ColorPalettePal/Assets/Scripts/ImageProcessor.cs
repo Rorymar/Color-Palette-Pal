70e0d8eefac3fbe5822e00e2bdcfd92576cbd04b
Rorymar <36866664+Rorymar@users.noreply.github.com>
2024-11-17 11:47:07 -0500

    Created first iterations of removal tools
    Created first versions of the removal tools, and also made changes in selection and displaying of color palette to support those new features.

--- ColorPalettePal/Assets/Scripts/ImageProcessor.cs
+++ ColorPalettePal/Assets/Scripts/ImageProcessor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,15 +16,16 @@ public class ImageProcessor : MonoBehaviour
     public TMP_InputField[] inputFields;
 
     [SerializeField]
-    GameObject regenButtonObj;
-    private Button regenButton;
-
-    [SerializeField]
-    GameObject saveButtonObj;
-    private Button saveButton;
+    GameObject[] buttonObjects;
+    private Button[] buttons;
 
     [SerializeField]
     ColorConverter cc;
+
+    //Create the variables for the banlists.
+    List<string> blackBanList;
+    List<string> whiteBanList;
+    List<string> grayBanList;
 
     // Start is called before the first frame update
     void Start()
@@ -34,10 +36,11 @@ public class ImageProcessor : MonoBehaviour
         }
         previousTexture = rawImage.texture;
 
-        if (regenButton == null){
-            regenButton = regenButtonObj.GetComponent<Button>();
+        buttons = new Button[buttonObjects.Length];
+        for(int i = 0; i < buttonObjects.Length; i++){
+            buttons[i] = buttonObjects[i].GetComponent<Button>();
+            buttons[i].interactable = false;
         }
-        regenButton.interactable = false;
         /*
         //Add when save implemented
         if (saveButton == null){
@@ -46,6 +49,45 @@ public class ImageProcessor : MonoBehaviour
         saveButton.interactable = false;
         */
 
+        //Create the Black Ban List
+        blackBanList = new List<string>();
+        for(int r = 0; r <= 30; r++){
+            for(int g = 0; g <= 30; g++){
+                for(int b = 0; b <= 30; b++){
+                    string hexColor = $"#{r:X2}{g:X2}{b:X2}";
+                    blackBanList.Add(hexColor);
+                }
+            }
+        }
+
+        //Create the White Ban List
+        whiteBanList = new List<string>();
+        for(int r = 225; r <= 255; r++){
+            for(int g = 225; g <= 255; g++){
+                for(int b = 225; b <= 255; b++){
+                    string hexColor = $"#{r:X2}{g:X2}{b:X2}";
+                    whiteBanList.Add(hexColor);
+                }
+            }
+        }
+
+        //Create the Gray Ban List
+        grayBanList = new List<string>();
+        int start = 125;
+        int end = 185;
+        for(int r = start; r <= end; r++){
+            for(int g = r-30; g <= r+30; g++){
+                for(int b = r-30; b <= r+30; b++){
+                    if(g >= 0 && g <= 255 && b >= 0 && b <= 255){
+                        if(Mathf.Abs(r-g) <= 30 && Mathf.Abs(g-b) <= 30 && Mathf.Abs(r - b) <= 30){
+                            string hexColor = "#"+r.ToString("X2")+g.ToString("X2")+b.ToString("X2");
+                            grayBanList.Add(hexColor);
+                        }
+                    }
+                }
+            }
+        }
+
         //DELETE WHEN CACHE IS SET UP(?)
         ResetColors();
     }
@@ -53,9 +95,9 @@ public class ImageProcessor : MonoBehaviour
     //Triggers when application quits
     void OnApplicationQuit()
     {
-        regenButton.interactable = false;
-        //TBA when Save is Implemented:
-        //saveButton.GetComponent<Button>().interactable = false;
+        for(int i = 0; i < buttonObjects.Length; i++){
+            buttons[i].interactable = false;
+        }
         ResetColors();
     }
 
@@ -76,9 +118,10 @@ public class ImageProcessor : MonoBehaviour
             //Call method to process texture
             Debug.Log("Image loaded! Beginning processing...");
 
-            regenButton.interactable = true;
-            //TBA when Save is Implemented:
-            //saveButton.interactable = true; 
+            for(int i = 0; i < buttonObjects.Length; i++){
+                buttons[i] = buttonObjects[i].GetComponent<Button>();
+                buttons[i].interactable = true;
+            }
             
             previousTexture = rawImage.texture;
             ProcessTexture((Texture2D)rawImage.texture);
@@ -110,14 +153,43 @@ public class ImageProcessor : MonoBehaviour
     //Regenerate the Palette, not producing the same colors as before.
     public void RegenColors(){
         Debug.Log("Generating a new Palette...");
-        List<Color> banList = new List<Color>();
+        List<string> banList = new List<string>();
         for (int i = 0; i < materials.Length; i++)
         {
-            banList.Add(materials[i].color);
+            banList.Add(cc.createHexFromColor(materials[i].color));
         }
         if(colorCounts != null){
             SelectColors(colorCounts,banList,randomizeTimes: 1);
         }
+    }
+
+    //Removes all blacks from palette
+    public void RemoveColorGroupBlack(){
+        List<Color> preselects = buildPreselects(blackBanList);
+        SelectColors(colorCounts,blackBanList,randomizeTimes: 1, preselects);
+    }
+
+    //Remove all whites from palette
+    public void RemoveColorGroupWhite(){
+        List<Color> preselects = buildPreselects(whiteBanList);
+        SelectColors(colorCounts,whiteBanList,randomizeTimes: 1,preselects);
+    }
+
+    //Remove all grays from palette
+    public void RemoveColorGroupGray(){
+        List<Color> preselects = buildPreselects(grayBanList);
+        SelectColors(colorCounts,grayBanList,randomizeTimes: 1, preselects);
+    }
+
+    //Builds the list of colors that don't need to be removed from the current line-up.
+    public List<Color> buildPreselects(List<string> banList){
+        List<Color> preselects = new List<Color>();
+        foreach(Material material in materials){
+            if(checkViability(cc.createHexFromColor(material.color),banList)){
+                preselects.Add(material.color);
+            }
+        }
+        return preselects;
     }
 
     //Simplifies the color information in a color
@@ -180,10 +252,12 @@ public class ImageProcessor : MonoBehaviour
                                           .ToList();
     }
 
-    private void SelectColors(List<Color> sortedColors, List<Color> banList = null, int randomizeTimes = 0, int colorsToGrab = 6){
+    //Selects the colors for the palette
+    private void SelectColors(List<Color> sortedColors, List<string> banList = null, int randomizeTimes = 0, List<Color> preselects = null){
         
         //Starting threshold and empty list
         float threshold = 0.3f;
+        int colorsToGrab = 6;
         List<Color> selectedColors = new List<Color>();
 
         //Random the color list.
@@ -196,10 +270,13 @@ public class ImageProcessor : MonoBehaviour
 
             //Clear the list
             selectedColors.Clear();
+            if(preselects != null){
+                selectedColors.AddRange(preselects);
+            }
 
             //Loop through the colors till you have six or run out
             foreach (var color in sortedColors){
-                if(banList != null && banList.Contains(color)){
+                if(banList != null && !checkViability(cc.createHexFromColor(color),banList)){
                     continue;
                 }
                 if(selectedColors.Count < colorsToGrab)
@@ -219,8 +296,22 @@ public class ImageProcessor : MonoBehaviour
             //If failed to complete a palette, lower the threshold and try again.
             threshold -= 0.05f;
         }
-        //Display the colors in the UI
         DisplayColors(selectedColors);
+    }
+
+    //Verifies if the current color is in the banned list of colors
+    private bool checkViability(string hexCode, List<string> BanList){
+        foreach(string otherHex in BanList){
+            if(hexCode.Equals(otherHex,StringComparison.OrdinalIgnoreCase)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Calculate the degree of similarity between two colors.
+    private float ColorDifference(Color a, Color b){
+        return Mathf.Sqrt(Mathf.Pow(a.r - b.r, 2) + Mathf.Pow(a.g-b.g,2)+Mathf.Pow(a.b-b.b,2));
     }
 
     //Randomizes the list of sorted colors to induce a degree of variance in results
@@ -249,9 +340,38 @@ public class ImageProcessor : MonoBehaviour
         //Assign the six colors
         Color blank = new Color();
         blank = Color.clear;
-        while(selectedColors.Count < materials.Length){
-            selectedColors.Add(blank);
+
+        //For the removals, order of extisting colors should be maintained
+        List<Color> reorderedColors = new List<Color>();
+        List<bool> used = new List<bool>(new bool[selectedColors.Count]);
+
+        for(int i = 0; i < materials.Length; i++){
+            bool matchFound = false;
+            for(int j = 0; j < selectedColors.Count; j++){
+                if(!used[j] && materials[i].color == selectedColors[j]){
+                    reorderedColors.Add(selectedColors[j]);
+                    used[j] = true;
+                    matchFound = true;
+                    break;
+                }
+            }
+            if(!matchFound){
+                reorderedColors.Add(blank);
+            }
         }
+        for(int i = 0; i < selectedColors.Count; i++){
+            if(!used[i]){
+                for(int j = 0; j < reorderedColors.Count; j++){
+                    if(reorderedColors[j] == blank){
+                        reorderedColors[j] = selectedColors[i];
+                    }
+                }
+            }
+        }
+
+        selectedColors = reorderedColors;
+
+        //Sets the colors
         for (int i = 0; i < materials.Length; i++)
         {
             if (i < selectedColors.Count)
@@ -261,11 +381,6 @@ public class ImageProcessor : MonoBehaviour
                 //Debug.Log($"Assigned Color: {selectedColors[i]} to Material: {materials[i].name}");
             }
         }
-    }
-
-    //Calculate the degree of similarity between two colors.
-    private float ColorDifference(Color a, Color b){
-        return Mathf.Sqrt(Mathf.Pow(a.r - b.r, 2) + Mathf.Pow(a.g-b.g,2)+Mathf.Pow(a.b-b.b,2));
     }
 
 }
