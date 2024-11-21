aace021272959c6e4721c0d095ec56907f333b07
Salteeeeee <aidan.rosenblatt@yahoo.com>
2024-11-21 00:21:52 -0500

    Palette History WIP
    - Created new class, PaletteHistory, to store and display previously generated palettes
    - Added UI elements to display palette history
    - Still need to implement buttons to hide and show the palette generator and palette history

---
+++ ColorPalettePal/Assets/Scripts/PaletteHistory.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PaletteHistory : MonoBehaviour
+{
+    //IMPORTANT NOTE: the length of histDisplays MUST be the same as histSize!!!
+    [SerializeField]
+    int histSize;
+
+    [SerializeField]
+    List<TMP_Text> histDisplays;
+
+    [SerializeField]
+    List<TMP_InputField> hexSources;
+
+    private List<List<string>> history;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        history = new List<List<string>>();
+        for(int i = 0; i < histSize; i++) {
+            history.Add(new List<string>(6));
+            for(int j = 0; j < 6; j++) {
+                history[i].Add("#000000");
+            }
+        }
+
+        displayHistory();
+        //print(history[0][0]);
+    }
+
+    //takes a list of six hex codes as input and adds the list to history, with new entries being added to the beginning
+    public void newEntry() {
+        List<string> newPal = new List<string>(6);
+        for(int i = 0; i < 6; i++) {
+            newPal.Add(hexSources[i].text);
+        }
+        
+        for(int i = 4; i > 0; i--) {
+            history[i] = history[i-1];
+        }
+        history[0] = newPal;
+        displayHistory();
+        return;
+    }
+
+    //display the hex codes stored in history
+    private void displayHistory() {
+        for(int i = 0; i < histSize; i++) {
+            string outStr = history[i][0];
+            for(int j = 1; j < 6; j++) {
+                outStr = outStr + ", " + history[i][j];
+            }
+            histDisplays[i].text = outStr;
+        }
+        return;
+    }
+}
