fa6ea65b84d2b56b857b60f03f338cb55ae3613d
Rorymar <36866664+Rorymar@users.noreply.github.com>
2024-11-21 20:57:54 -0500

    Palette History created
    Created Palette History for Palette From Scratch

--- ColorPalettePal/Assets/Scripts/PaletteHistory.cs
+++ ColorPalettePal/Assets/Scripts/PaletteHistory.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using TMPro;
 
@@ -27,12 +28,13 @@ public class PaletteHistory : MonoBehaviour
     void Start()
     {
         history = new List<List<string>>();
-        for(int i = 0; i < histSize; i++) {
-            history.Add(new List<string>(6));
-            for(int j = 0; j < 6; j++) {
-                history[i].Add("#000000");
-            }
+        history.Add(new List<string>(1));
+        history[0].Add("You don't have any palettes yet!");
+        for(int i = 1; i < histSize; i++) {
+            history.Add(new List<string>(1));
+            history[i].Add("");
         }
+        
 
         displayHistory();
         //print(history[0][0]);
@@ -40,12 +42,25 @@ public class PaletteHistory : MonoBehaviour
 
     //takes a list of six hex codes as input and adds the list to history, with new entries being added to the beginning
     public void newEntry() {
-        List<string> newPal = new List<string>(6);
+        List<string> newPal = new List<string>();
         for(int i = 0; i < 6; i++) {
+            if(hexSources[i].text == "Hex Code" || newPal.Contains(hexSources[i].text)){
+                continue;
+            }
             newPal.Add(hexSources[i].text);
         }
-        
-        for(int i = 4; i > 0; i--) {
+
+        //Used to retrieve extant hex group if it exists
+        List<string> sortednewPal = newPal.OrderBy(s => s).ToList();
+        int index = history.FindIndex(l => l.OrderBy(s => s).SequenceEqual(sortednewPal));
+        if(index < 0){
+            index = history.Count - 1;
+        }
+        //Shift down and update
+        for(int i = index; i > 0; i--) {
+            if(history[i-1][0] == "You don't have any palettes yet!"){
+                continue;
+            }
             history[i] = history[i-1];
         }
         history[0] = newPal;
@@ -62,7 +77,7 @@ public class PaletteHistory : MonoBehaviour
     private void displayHistory() {
         for(int i = 0; i < histSize; i++) {
             string outStr = history[i][0];
-            for(int j = 1; j < 6; j++) {
+            for(int j = 1; j < history[i].Count; j++) {
                 outStr = outStr + ", " + history[i][j];
             }
             histDisplays[i].text = outStr;
